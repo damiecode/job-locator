ab80d5dc7332fc5d22ab1c4df2cd0a97dff39fc9
damiecode <codenlyn@gmail.com>
2020-11-10 17:16:05 +0100

    Add header and footer to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  Switch, Route,
+} from 'react-router-dom';
+import '../assets/css/App.css';
+import AllCategories from '../containers/home/home';
+import Header from './header';
+import Footer from './footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Header />
+    <Switch>
+    </Switch>
+    <Footer />
+  </div>
+);
 
 export default App;
